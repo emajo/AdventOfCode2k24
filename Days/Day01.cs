4ac63305194f28a82a1c2a3e7edcbdd3656e68a9
emanuele <emanuele.saccomandi@gmail.com>
2024-12-01 11:30:45 +0100

    day 1

---
+++ Days/Day01.cs
@@ -0,0 +1,46 @@
+﻿class Day01
+{
+    public static long Part(bool partOne = true)
+    {
+        var sr = new StreamReader("inputs/dayone.txt");
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine() ?? "";
+            var values = line.Split("   ").Select(int.Parse).ToList();
+
+            leftList.Add(values.First());
+            rightList.Add(values.Last());
+
+        }
+        sr.Close();
+
+        leftList = leftList.Order().ToList();
+        rightList = rightList.Order().ToList();
+
+        int tot = 0;
+        
+        if(partOne)
+        {
+            for (int i = 0;i < leftList.Count; i++)
+            {
+                tot += Math.Abs(leftList[i] - rightList[i]);
+            }
+        }
+
+        if(!partOne)
+        {
+            var intersections = rightList
+                .Intersect(leftList).ToList();
+
+            for (int z = 0;z < intersections.Count;z++)
+            {
+                tot += intersections[z] * rightList.Count(i => i == intersections[z]);
+            }
+        }
+
+        return tot;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Insert the day bro... ");
+var day = Console.ReadLine();
+
+switch (day)
+{
+    case "1":
+        Console.WriteLine(Day01.Part(true));
+        Console.WriteLine(Day01.Part(false));
+        break;
+
+    default:
+        Console.WriteLine("Default");
+        break;
+}
