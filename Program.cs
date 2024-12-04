1b87a686cfb5c677810bcff55a605d43347fc4a3
Emanuele Saccomandi <emanuele.saccomandi@gmail.com>
2024-12-04 20:01:07 +0100

    program

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ switch (day)
         Console.WriteLine(Day01.Part(true));
         Console.WriteLine(Day01.Part(false));
         break;
+    case "2":
+        Console.WriteLine(Day02.Part(true));
+        Console.WriteLine(Day02.Part(false));
+        break;
+    case "3":
+        Console.WriteLine(Day03.Part(true));
+        Console.WriteLine(Day03.Part(false));
+        break;
+    case "4":
+        Console.WriteLine(Day04.Part(true));
+        Console.WriteLine(Day04.Part(false));
+        break;
 
     default:
         Console.WriteLine("Default");
